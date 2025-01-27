cb1070205b12b8efeecd08ba58777e5dd2491d02
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-01-27 14:20:23 +0100

    Problem solved with \' instead of \´
    The script was failing because \' is not the same that \´ and it wasn't
    detecting the env variables.

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -31,25 +31,25 @@ fi
 # Start MariaDB service
 service mariadb start;
 
-# Create database if it doesn't exist
-mysql -e "CREATE DATABASE IF NOT EXISTS \'$SQL_DATABASE\';"
+# Create the database if it doesn't exist
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;"
 
-# Create user if it doesn't exist
-mysql -e "CREATE USER IF NOT EXISTS \'$SQL_USER\'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+# Create the user if it doesn't exist
+mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'localhost' IDENTIFIED BY '$SQL_PASSWORD';"
 
 # Grant all privileges to the user on the specified database
-mysql -e "GRANT ALL PRIVILEGES ON \'$SQL_DATABASE\'.* TO \'$SQL_USER\'@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 
 # Change the root password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
 
-# Restart privileges to apply changes
+# Apply privilege changes
 mysql -e "FLUSH PRIVILEGES;"
 
 # Shutdown MySQL to ensure proper initialization
-mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
 
 service mariadb stop;
 
 # Start MariaDB using mysqld (preferred for container environment)
-exec mysqld_safe
+exec mariadbd
