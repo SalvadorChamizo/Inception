5d323698a078f0a2b8a88f52bcaeb36553c28ea0
schamizo <salvadorchamizo@gmail.com>
2025-02-04 12:38:47 +0100

    Initialitation script for adminer

--- srcs/requirements/bonus/adminer/adminer_init.sh
+++ srcs/requirements/bonus/adminer/adminer_init.sh
@@ -1,3 +1,9 @@
 PHP_VERSION=$(php -r "echo PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;")
 
-php-fpm${PHP_VERSION} -F 0.0.0.0:9000
+mkdir -p /run/php
+
+mv ./www.conf /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
+
+#sed -i "s|listen = /run/php/php${PHP_VERSION}-fpm.sock|listen = 9000|g" etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
+
+php-fpm${PHP_VERSION} -F
