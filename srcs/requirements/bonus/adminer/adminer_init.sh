01fe25eaa106fbd9c639fbdcad6bd0c529d7a854
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-02-04 12:40:42 +0100

    Removed line from adminer_init.sh

--- srcs/requirements/bonus/adminer/adminer_init.sh
+++ srcs/requirements/bonus/adminer/adminer_init.sh
@@ -4,6 +4,4 @@ mkdir -p /run/php
 
 mv ./www.conf /etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
 
-#sed -i "s|listen = /run/php/php${PHP_VERSION}-fpm.sock|listen = 9000|g" etc/php/${PHP_VERSION}/fpm/pool.d/www.conf
-
 php-fpm${PHP_VERSION} -F
